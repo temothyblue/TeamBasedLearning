717b963c7646c89251f05d725af3a019204170ca
jquezadarios <jquezadarios@gmail.com>
2018-11-06 10:11:21 -0300

    Auto stash before rebase of "master"

--- TeamBasedLearning/resources/views/funcionamiento_sala/message.php
+++ TeamBasedLearning/resources/views/funcionamiento_sala/message.php
@@ -4,9 +4,10 @@
 	<title></title>
 </head>
 <body>
+
 	<form method="post" action="view.php">
-		<input type="text" name="msj">
-		<input type="submit" name="ok">
+		<input type="text" name="msj" placeholder="Pregunta">
+		<input  class="btn btn-primary" type="submit"  name="ok"> 
 		<input type="hidden" name="sal" value=<?php echo $sala ?>>
 		<input type="hidden" name="idusr" value=<?php echo $idusr ?>>
 		<input type="hidden" name="nameusr" value=<?php echo $user ?>>
